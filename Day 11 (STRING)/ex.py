f76a85026f3233bcd991e211125dd66706d98a93
Yash Tingare <yashtingare2007@gmail.com>
2026-05-07 10:40:06 +0530

    Add files via upload

---
+++ Day 11 (STRING)/ex.py
@@ -0,0 +1,5 @@
+# Example of multiplu line in string '''      '''
+string = '''Yash said "You sre looking bueatyfull"
+          She Said "Hahahah Yash Thank you"
+          Yash Said " I am just kidding"'''
+print(string)
